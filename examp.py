59c9658f988b89b1e58128db013d7b721f713d7f
Leotiv-Vibs <sobaka.ty.my@gmail.com>
2019-11-23 13:12:48 +0300

    MS

---
+++ examp.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+import os
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.advanced_activations import ELU
+from keras.layers.core import Activation, Flatten, Dropout, Dense
+from keras.optimizers import RMSprop, SGD, Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from keras import regularizers
+from keras.regularizers import l1
+num_classes = 7
+img_rows, img_cols = 48, 48
+batch_size = 512
+
+train_data_dir = './fer2013/train'
+validation_data_dir = './fer2013/validation'
